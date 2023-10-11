67dca11e92bf932878198a38976c9a569d39c6a2
ActualRML <rmlilipaly@gmail.com>
2023-10-11 13:09:41 +0700

    asd

---
+++ frontend/src/pages/AddProduct.jsx
@@ -0,0 +1,11 @@
+import { Container, Stack, Text } from "@chakra-ui/react";
+
+const AddProductPage = () => {
+  return;
+  <Container>
+    <Stack spacing="24px" textAlign="center">
+      <Text fontsize="36px">Create Product</Text>
+    </Stack>
+  </Container>;
+};
+export default AddProductPage;
