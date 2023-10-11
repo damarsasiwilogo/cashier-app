0b54673b4dd3f8c635fcf9c29c4cff3819b9a9e4
ActualRML <rmlilipaly@gmail.com>
2023-10-11 14:02:49 +0700

    fdmla;

--- frontend/src/pages/AddProduct.jsx
+++ frontend/src/pages/AddProduct.jsx
@@ -1,11 +1,37 @@
-import { Container, Stack, Text } from "@chakra-ui/react";
+import {
+  Container,
+  FormControl,
+  Input,
+  Stack,
+  Text,
+  Switch,
+  FormLabel,
+  Select,
+  Button,
+} from "@chakra-ui/react";
 
 const AddProductPage = () => {
-  return;
-  <Container>
-    <Stack spacing="24px" textAlign="center">
-      <Text fontsize="36px">Create Product</Text>
-    </Stack>
-  </Container>;
+  return (
+    <Container>
+      <Stack spacing="24px" textAlign="center">
+        <Text fontsize="36px">Create Product</Text>
+        <FormControl>
+          <Input placeholder="name" />
+          <Input placeholder="price" />
+          <Select placeholder="Category">
+            <option value="option1">Option 1</option>
+          </Select>
+          <Input placeholder="description" />
+          <Button colorScheme="teal" variant="outline">
+            Add Image
+          </Button>
+          <FormLabel htmlFor="email-alerts" mb="0">
+            Active
+          </FormLabel>
+          <Switch id="active" />
+        </FormControl>
+      </Stack>
+    </Container>
+  );
 };
 export default AddProductPage;
